4b02f4e0b7608d9940856f9034254388124d7ccb
quellus <quellus7@gmail.com>
2020-03-14 19:09:33 -0600

    move map related code to its own class

--- core/src/com/quellus/libgdxgame/Game.java
+++ core/src/com/quellus/libgdxgame/Game.java
@@ -2,12 +2,6 @@ package com.quellus.libgdxgame;
 
 import java.util.ArrayList;
 
-import java.io.*;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.files.FileHandle;
-
 import com.quellus.libgdxgame.GameLogic;
 import com.quellus.libgdxgame.entities.Enemy;
 import com.quellus.libgdxgame.entities.towers.Tower;
@@ -19,11 +13,10 @@ public class Game {
 	private ArrayList<Tower> towers = new ArrayList<Tower>();
 	private ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 
-	private Coordinate<Integer>[] map;
+	private Map map;
 
 	public Game(String filename) {
-		FileHandle mapFile = Gdx.files.internal(filename);
-		parseMap(mapFile);
+		map = new Map(filename);
 	}
 
 	public ArrayList<Enemy> getEnemies()  {
@@ -59,80 +52,7 @@ public class Game {
 	}
 	
 	public Coordinate<Integer>[] getMap() {
-		return map;
-	}
-
-	private boolean parseMap(FileHandle file) {
-		String fileString = file.readString();
-		String[] lines = fileString.split("\\r?\\n");
-		ArrayList<Coordinate<Integer>> coordList = new ArrayList<Coordinate<Integer>>();
-		for (int i = 0; i < lines.length; i++) {
-			String[] coords = lines[i].split(" ");
-			if (coords.length != 2) {
-				Gdx.app.error("TowerDefense","Failed to parse map file");
-				return false;
-			}
-			Coordinate<Integer> coord = new Coordinate<Integer>(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
-			coordList.add(coord);
-		}
-
-		coordList = waypointsToTiles(coordList);
-		if (coordList == null) {
-			Gdx.app.error("TowerDefense", "waypointsToTiles failed");
-			return false;
-		}
-
-		Coordinate<Integer>[] map = new Coordinate[coordList.size()];
-		this.map = coordList.toArray(map);
-		debugMap();
-		return true;
-	}
-
-	private ArrayList<Coordinate<Integer>> waypointsToTiles(ArrayList<Coordinate<Integer>> map) {
-		ArrayList<Coordinate<Integer>> newMap = new ArrayList<Coordinate<Integer>>();
-		for (int i = 0; i < map.size(); i++) {
-			Coordinate<Integer> currCoords = map.get(i);
-			Coordinate<Integer> nextPointCoords = null;
-
-			int x = currCoords.getX();
-			int y = currCoords.getY();
-
-			if (i + 1 < map.size()) {
-				nextPointCoords = map.get(i + 1);
-			} else {
-				newMap.add(new Coordinate<Integer>(x, y));
-				return newMap;
-			}
-
-			int pointX = nextPointCoords.getX();
-			int pointY = nextPointCoords.getY();
-
-			while (pointX != x || pointY != y) {
-				newMap.add(new Coordinate<Integer>(x, y));
-				if (pointX == x) {
-					if (pointY > y) {
-						y++;
-					} else {
-						y--;
-					}
-				} else {
-					if (pointX > x) {
-						x++;
-					} else {
-						x--;
-					}
-				}
-			}
-		}
-		return newMap;
-	}
-
-	private void debugMap() {
-		String mapStr = "";
-		for (int i = 0; i < map.length; i++) {
-			mapStr += ", " + map[i].toString();
-		}
-		Gdx.app.log("TowerDefense", mapStr);
+		return map.getMapTiles();
 	}
 
 }
---
+++ core/src/com/quellus/libgdxgame/Map.java
@@ -0,0 +1,95 @@
+package com.quellus.libgdxgame;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Application;
+
+public class Map {
+
+  private Coordinate<Integer>[] mapTiles;
+
+  public Map (String filename) {
+		FileHandle mapFile = Gdx.files.internal(filename);
+    parseMap(mapFile);
+		debugMap(mapTiles);
+  }
+
+  public Coordinate<Integer>[] getMapTiles() {
+      return mapTiles;
+  }
+
+  private boolean parseMap(FileHandle file) {
+		String fileString = file.readString();
+		String[] lines = fileString.split("\\r?\\n");
+		ArrayList<Coordinate<Integer>> coordList = new ArrayList<Coordinate<Integer>>();
+		for (int i = 0; i < lines.length; i++) {
+			String[] coords = lines[i].split(" ");
+			if (coords.length != 2) {
+				Gdx.app.error("TowerDefense","Failed to parse map file");
+				return false;
+			}
+			Coordinate<Integer> coord = new Coordinate<Integer>(Integer.parseInt(coords[0]), Integer.parseInt(coords[1]));
+			coordList.add(coord);
+		}
+
+		coordList = waypointsToTiles(coordList);
+		if (coordList == null) {
+			Gdx.app.error("TowerDefense", "waypointsToTiles failed");
+			return false;
+		}
+
+		Coordinate<Integer>[] map = new Coordinate[coordList.size()];
+		mapTiles = coordList.toArray(map);
+		return true;
+	}
+
+	private ArrayList<Coordinate<Integer>> waypointsToTiles(ArrayList<Coordinate<Integer>> map) {
+		ArrayList<Coordinate<Integer>> newMap = new ArrayList<Coordinate<Integer>>();
+		for (int i = 0; i < map.size(); i++) {
+			Coordinate<Integer> currCoords = map.get(i);
+			Coordinate<Integer> nextPointCoords = null;
+
+			int x = currCoords.getX();
+			int y = currCoords.getY();
+
+			if (i + 1 < map.size()) {
+				nextPointCoords = map.get(i + 1);
+			} else {
+				newMap.add(new Coordinate<Integer>(x, y));
+				return newMap;
+			}
+
+			int pointX = nextPointCoords.getX();
+			int pointY = nextPointCoords.getY();
+
+			while (pointX != x || pointY != y) {
+				newMap.add(new Coordinate<Integer>(x, y));
+				if (pointX == x) {
+					if (pointY > y) {
+						y++;
+					} else {
+						y--;
+					}
+				} else {
+					if (pointX > x) {
+						x++;
+					} else {
+						x--;
+					}
+				}
+			}
+		}
+		return newMap;
+	}
+
+	private void debugMap(Coordinate<Integer>[] map) {
+		String mapStr = "";
+		for (int i = 0; i < map.length; i++) {
+			mapStr += ", " + map[i].toString();
+		}
+		Gdx.app.log("TowerDefense", mapStr);
+	}
+
+}
