53f048a8a7ccbbff50af2d7d8b190a4683713ceb
quellus <quellus7@gmail.com>
2020-04-22 22:01:05 -0600

    fixed cooldowns not updating sometimes

--- core/src/com/quellus/libgdxgame/entities/towers/TurretTower.java
+++ core/src/com/quellus/libgdxgame/entities/towers/TurretTower.java
@@ -4,21 +4,18 @@ import com.quellus.libgdxgame.entities.towers.Tower;
 import com.quellus.libgdxgame.entities.towers.TowerEnum;
 import com.quellus.libgdxgame.entities.Enemy;
 import com.quellus.libgdxgame.entities.projectiles.Bullet;
+import com.quellus.libgdxgame.entities.projectiles.Projectile;
 
 public class TurretTower extends Tower {
 
 	public TurretTower(int x, int y) {
 		super(x, y, 3.2f);
+		cooldown = 10;
 		type = TowerEnum.TURRET;
 	}
 
-	public Bullet attack(Enemy enemy) {
-		boolean canAttack = currentCooldown == 0;
-		updateCooldown();
-		if (canAttack) {
+	protected Projectile spawnProjectile(Enemy enemy) {
 			return new Bullet(this, enemy, damage);
-		}
-		return null;
 	}
 
 }
